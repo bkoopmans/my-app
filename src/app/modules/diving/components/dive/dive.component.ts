95dd46616d051cb4b35a1d825dac0743216c28be
Brandon Koopmans <bradlkooper@gmail.com>
2020-10-27 11:43:19 -0700

    remove key

--- src/app/modules/diving/components/dive/dive.component.ts
+++ src/app/modules/diving/components/dive/dive.component.ts
@@ -10,7 +10,7 @@ const CosmosClient = require("@azure/cosmos").CosmosClient;
 export class DiveComponent implements OnInit {
   config: any = {
     endpoint: "https://my-app-db.documents.azure.com:443/",
-    key: "IeB85Lz5EzFchGyG7izHzn1sv6jRsVCfBZWfDFSfXO71lV9shGDeHVePyrRpDRkng891Ba4aRjRw3rk2uXdlFw==",
+    key: "",
     databaseId: "Dives",
     containerId: "MyCollection",
     partitionKey: { kind: "Hash", paths: ["/location"] }
--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,6 @@
 const config = {
     endpoint: "https://my-app-db.documents.azure.com:443/",
-    key: "IeB85Lz5EzFchGyG7izHzn1sv6jRsVCfBZWfDFSfXO71lV9shGDeHVePyrRpDRkng891Ba4aRjRw3rk2uXdlFw==",
+    key: "",
     databaseId: "Dives",
     containerId: "MyCollection",
     partitionKey: { kind: "Hash", paths: ["/location"] }
