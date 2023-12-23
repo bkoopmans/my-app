fcc1ddca6e9c04fb2c9ddbc58fd5e2464190a951
Brandon Koopmans <blkooper@hotmail.com>
2023-12-22 22:32:48 -0800

    decoder stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { ResumeModule } from './modules/resume/resume.module';
 import { DivingModule } from './modules/diving/diving.module';
 import { IfAdventureModule } from './modules/if-adventure/if-adventure.module';
-
+import { DecoderModule } from './modules/decoder/decoder.module';
 
 
 
@@ -24,7 +24,8 @@ import { IfAdventureModule } from './modules/if-adventure/if-adventure.module';
     ReactiveFormsModule,
     ResumeModule,
     DivingModule,
-    IfAdventureModule
+    IfAdventureModule,
+    DecoderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
